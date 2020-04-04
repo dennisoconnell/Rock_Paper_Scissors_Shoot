d38991615099649a342e9f3cb5567da4a6b27059
Dennis O'Connell <doconnell@scu.edu>
2020-04-04 14:24:05 -0700

    created barebones game in javascript
    the game can now recognize what the user input is, what the computer input is, and who wins.

--- app.js
+++ app.js
@@ -8,6 +8,57 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
-paper_div.addEventListener('click', function() {
-    console.log("PAPER!");
-})
+
+function main(){
+    
+    rock_div.addEventListener('click', function() {
+        console.log("ROCK!");
+        game("r");
+    })
+
+    paper_div.addEventListener('click', function() {
+        console.log("PAPER!");
+        game("p");
+    })
+
+    scissors_div.addEventListener('click', function() {
+        console.log("SCISSORS!");
+        game("s");
+    })
+}
+
+
+function getComputerChoice(){
+    const choices = ['r','p','s'];
+    const randomNumber = Math.floor(Math.random()*3);
+    return choices[randomNumber];
+}
+
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    console.log(userChoice);
+    console.log(computerChoice);
+
+
+    switch (userChoice + computerChoice){
+        case "rs":
+        case "pr":
+        case "sp":
+            console.log("The User Won!");
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            console.log("The User Lost.");
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            console.log("It's a tie!");
+            break;
+        
+    }
+}
+
+main();
