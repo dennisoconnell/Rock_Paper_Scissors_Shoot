8a92dc5ce9c5dd591382b222e754eb04af5a5716
Dennis O'Connell <doconnell@scu.edu>
2020-04-17 23:27:56 -0700

    reformatted with prettier, added youtube link
    reformatted with prettier, added youtube link

--- app.js
+++ app.js
@@ -9,82 +9,80 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+function main() {
+  rock_div.addEventListener("click", function () {
+    game("r");
+  });
 
-function main(){
-    
-    rock_div.addEventListener('click', function() {
-        game("r");
-    })
+  paper_div.addEventListener("click", function () {
+    game("p");
+  });
 
-    paper_div.addEventListener('click', function() {
-        game("p");
-    })
-
-    scissors_div.addEventListener('click', function() {
-        game("s");
-    })
+  scissors_div.addEventListener("click", function () {
+    game("s");
+  });
 }
 
-
-function getComputerChoice(){
-    const choices = ['r','p','s'];
-    const randomNumber = Math.floor(Math.random()*3);
-    return choices[randomNumber];
+function getComputerChoice() {
+  const choices = ["r", "p", "s"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
 }
 
-function win(userChoice, computerChoice){
-    userScore++;
-    userScore_span.innerHTML = userScore;
-    computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = userChoice + " beats " + computerChoice + ". You win!";
-    change_green(userChoice);
-    setTimeout(change_back(userChoice), 2000);
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = userChoice + " beats " + computerChoice + ". You win!";
+  change_green(userChoice);
+  setTimeout(change_back(userChoice), 2000);
 }
 
-    function change_green(userChoice){
-        console.log("TEST");
-        document.getElementById(userChoice).style.border = "8px solid green";
-        
-    }
-    function change_back(userChoice){
-        console.log("TEST");
-        document.getElementById(userChoice).style.border = "2px solid white";
-    }
-
-function lose(userChoice, computerChoice){
-    computerScore++;
-    userScore_span.innerHTML = userScore;
-    computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = userChoice + " loses to " + computerChoice + ". You lose!";
-    document.getElementById(userChoice).style.border = "8px solid red";
+function change_green(userChoice) {
+  console.log("TEST");
+  document.getElementById(userChoice).style.border = "8px solid green";
+}
+function change_back(userChoice) {
+  console.log("TEST");
+  document.getElementById(userChoice).style.border = "2px solid white";
 }
 
-function tie(userChoice, computerChoice){
-    result_p.innerHTML = userChoice + " ties with " + computerChoice + ". You tied!";
+function lose(userChoice, computerChoice) {
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML =
+    userChoice + " loses to " + computerChoice + ". You lose!";
+  document.getElementById(userChoice).style.border = "8px solid red";
 }
 
-function game(userChoice){
-    const computerChoice = getComputerChoice();
+function tie(userChoice, computerChoice) {
+  result_p.innerHTML =
+    userChoice + " ties with " + computerChoice + ". You tied!";
+}
 
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
 
-    switch (userChoice + computerChoice){
-        case "rs":
-        case "pr":
-        case "sp":
-            win(userChoice, computerChoice);
-            break;
-        case "rp":
-        case "ps":
-        case "sr":
-            lose(userChoice, computerChoice)
-            break;
-        case "rr":
-        case "pp":
-        case "ss":
-            tie(userChoice, computerChoice);
-            break;
-        
-    }
+  switch (userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice, computerChoice);
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      lose(userChoice, computerChoice);
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      tie(userChoice, computerChoice);
+      break;
+  }
 }
 
 main();
+
+//https://www.youtube.com/watch?v=jaVNP3nIAv0
